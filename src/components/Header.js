2f1d415c75e9b29580338d379f62c1df1cbcc60f
Hans Schroeder <hajschroeder@gmail.com>
2022-08-20 11:50:26 -0700

    include header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import iceCreamGround from "./../img/creamy.jpg";
 
 function Header() {
   return (
-    <h1>Inventory Tracker</h1>
+    <React.Fragment>
+      <h1>Inventory Tracker</h1>
+      <img src={iceCreamGround} alt="happy ice cream" />
+    </React.Fragment>
   );
 }
 
