1e65a846e865777d366a418791bb6d73bbb498ca
Hans Schroeder <hajschroeder@gmail.com>
2022-08-20 11:02:30 -0700

    create components directory. Move app.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Ice Cream Tracker!</h1>
+      <p>name</p>
+      <p><em>origin</em></p>
+      <p><b>price</b></p>
+      <p><em>flavor</em></p>
+      <hr/>
+    </React.Fragment>
+  )
+}
+
+export default App;
