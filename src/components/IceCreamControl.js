479ac3057fe0f42ad435b5184a9b344a3764950c
Hans Schroeder <hajschroeder@gmail.com>
2022-08-20 11:59:17 -0700

    include foundation code for Form and Control

---
+++ src/components/IceCreamControl.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NewIceCreamForm from "./NewIceCreamForm";
+import IceCreamList from "./IceCreamList";
+
+class IceCreamControl extends React.Component {
+  constructor(props){
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default IceCreamControl;
---
+++ src/components/NewIceCreamForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewIceCreamForm(props){
+  return (
+    <React.Fragment>
+      <h3>form placeholder</h3>
+    </React.Fragment>
+  )
+}
+
+export default NewIceCreamForm;
